57c3b2e2567e674838c16734d42c8d00de2973b3
renskrauweel <renskrauweel@hotmail.com>
2018-03-21 20:25:00 +0100

    Added question/answer Data + start on sortinghat + improvements on header.cshtml and homepage.cshtml

--- App/App_Code/SortingHat/Answer.cs
+++ App/App_Code/SortingHat/Answer.cs
@@ -5,13 +5,15 @@ using System.Web;
 
 public class Answer
 {
+    private int answer_id;
     private int question_id;
     private string answer;
     private string house;
     private int points;
 
-    public Answer(int question_id, string answer, string house, int points)
+    public Answer(int answer_id, int question_id, string answer, string house, int points)
     {
+        this.answer_id = answer_id;
         this.question_id = question_id;
         this.answer = answer;
         this.house = house;
@@ -19,6 +21,10 @@ public class Answer
     }
 
     // Getters
+    public int GetAnswerId()
+    {
+        return this.answer_id;
+    }
     public int GetQuestionId()
     {
         return this.question_id;
--- App/App_Code/SortingHat/Data.cs
+++ App/App_Code/SortingHat/Data.cs
@@ -5,36 +5,56 @@ using System.Web;
 
 public class Data
 {
-    private List<Question> questions = new List<Question>
+    private static List<Question> questions = new List<Question>
     {
+        /*
+         * Parameter order:
+         * int question_id | Unique, Auto increment
+         * string question
+         */
         new Question(1, "Which house do you prefer?"),
         new Question(2, "What's your favorite fruit?"),
+        new Question(3, "Who is the bad guy?"),
     };
 
-    private List<Answer> answers = new List<Answer>
+    private static List<Answer> answers = new List<Answer>
     {
-        new Answer(1, "Gryffindor", "Gryffindor", 5),
-        new Answer(1, "Hufflepuff", "Hufflepuff", 5),
-        new Answer(1, "Ravenclaw", "Ravenclaw", 5),
-        new Answer(1, "Slytherin", "Slytherin", 5),
+        /*
+         * Parameter order:
+         * int answer_id | Unique, Auto increment
+         * int question_id | Belonging question
+         * string answer | The answer
+         * string house | House where the points of the answer go to
+         * int points | Number of points
+         * string question
+         */
+        new Answer(1, 1, "Gryffindor", "Gryffindor", 5),
+        new Answer(2, 1, "Hufflepuff", "Hufflepuff", 5),
+        new Answer(3, 1, "Ravenclaw", "Ravenclaw", 5),
+        new Answer(4, 1, "Slytherin", "Slytherin", 5),
 
-        new Answer(2, "Apples", "Gryffindor", 5),
-        new Answer(2, "Banana's", "Hufflepuff", 3),
-        new Answer(2, "Kiwi's", "Ravenclaw", -2),
-        new Answer(2, "Grapes", "Slytherin", 1),
+        new Answer(5, 2, "Apples", "Gryffindor", 5),
+        new Answer(6, 2, "Banana's", "Hufflepuff", 3),
+        new Answer(7, 2, "Kiwi's", "Ravenclaw", -2),
+        new Answer(8, 2, "Grapes", "Slytherin", 1),
+
+        new Answer(9, 3, "Hagrid", "Gryffindor", 5),
+        new Answer(10, 3, "Snape", "Hufflepuff", 5),
+        new Answer(11, 3, "Dumbledore", "Ravenclaw", 5),
+        new Answer(12, 3, "Voldemort", "Slytherin", 5),
     };
 
     // Get all questions
-    public List<Question> GetQuestions()
+    public static List<Question> GetQuestions()
     {
-        return this.questions;
+        return Data.questions;
     }
 
     // Get question's answers by question_id
-    public List<Answer> GetAnswers(int question_id)
+    public static List<Answer> GetAnswers(int question_id)
     {
         List<Answer> answers = new List<Answer>();
-        foreach (Answer answer in this.answers)
+        foreach (Answer answer in Data.answers)
         {
             if (answer.GetQuestionId() == question_id)
             {
