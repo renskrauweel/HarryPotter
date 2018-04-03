2f2f4611206683aa3f789e9d887245a075bbd037
renskrauweel <renskrauweel@hotmail.com>
2018-04-03 13:57:50 +0200

    Progress on homework

--- App/App_Code/Homework/Question.cs
+++ App/App_Code/Homework/Question.cs
@@ -11,12 +11,26 @@ namespace Homework
         private int question_id;
         private string question;
         private int homework_id;
+        private List<Answer> answers = new List<Answer>();
 
         public Question(int question_id, string question, int homework_id)
         {
             this.question_id = question_id;
             this.question = question;
             this.homework_id = homework_id;
+
+            // Fetch answers from DB
+            var db = Database.Open("HarryPotter");
+            var rows = db.Query("SELECT * FROM answers WHERE question_id = @0", this.question_id);
+            db.Dispose();
+
+            foreach (var row in rows)
+            {
+                int answerId = (int)row["answer_id"];
+                int questionId = (int)row["question_id"];
+                int points = (int)row["points"];
+                this.answers.Add(new Answer(answerId, questionId, row["answer"], points));
+            }
         }
 
         // Getters
@@ -31,6 +45,10 @@ namespace Homework
         public int GetHomeworkId()
         {
             return this.homework_id;
+        }
+        public List<Answer> GetAnswers()
+        {
+            return this.answers;
         }
 
         // Insert question into DB
