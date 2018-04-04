ddf1bb0b60f3d98ba952a94904bca358f062925e
Rens thuis <renskrauweel@hotmail.com>
2018-04-04 15:47:42 +0200

    Housecup module done

--- App/App_Code/Housecup.cs
+++ App/App_Code/Housecup.cs
@@ -17,6 +17,20 @@ public class Housecup
         this.points = points;
     }
 
+    // Getters
+    public int GetYear()
+    {
+        return this.year;
+    }
+    public int GetHouseId()
+    {
+        return this.house_id;
+    }
+    public int GetPoints()
+    {
+        return this.points;
+    }
+
     public static bool LastYearExists()
     {
         var db = Database.Open("HarryPotter");
@@ -38,5 +52,34 @@ public class Housecup
         var db = Database.Open("HarryPotter");
         db.Execute("INSERT INTO housecup (year, house_id, points) VALUES (@0, @1, @2)", (DateTime.Now.Year - 1), houseId, housePointPair[houseId]);
         db.Dispose();
+    }
+
+    // Get all housecups
+    public static List<Housecup> GetHousecups()
+    {
+        var db = Database.Open("HarryPotter");
+        var rows = db.Query("SELECT * FROM housecup ORDER BY year DESC");
+        db.Dispose();
+
+        List<Housecup> housecups = new List<Housecup>();
+        foreach (var row in rows)
+        {
+            int year = (int)row["year"];
+            int houseId = (int)row["house_id"];
+            int points = (int)row["points"];
+            housecups.Add(new Housecup(year, houseId, points));
+        }
+
+        return housecups;
+    }
+
+    // Fetch house name
+    public string GetHouseName()
+    {
+        var db = Database.Open("HarryPotter");
+        string housename = db.QueryValue("SELECT house_name FROM houses WHERE house_id = @0", this.house_id);
+        db.Dispose();
+
+        return housename;
     }
 }
