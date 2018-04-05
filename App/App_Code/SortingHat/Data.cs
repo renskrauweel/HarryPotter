e4364dbab52dc6d6db4e7ea30651bc48a823e181
dantheman0125 <danielroskam@gmail.com>
2018-04-05 12:00:08 +0200

    changes

--- App/App_Code/SortingHat/Data.cs
+++ App/App_Code/SortingHat/Data.cs
@@ -13,12 +13,12 @@ public class Data
          * string question
          */
         new Question(1, "1. What animal do you associate yourself with the most?"),
-        new Question(2, "2. A Student went into the Dark Woods, because there is a tale about a mystical sword being there. You just heared that she has been gone now for the past 2 hours. " +
+        new Question(2, "2. You've just heard that a girl has been lost in the Dark Woods for over 2 hours. There is also a mythical sword hidden in the Dark Woods. " +
             "What will you do?"),
         new Question(3, "3. What would you do on your first day of Hogwarts?"),
         new Question(4, "4. Who of these people do you trust the most?"),
         new Question(5, "5. There are 4 relics in front of you which one would you pick?"),
-        new Question(6, "6. What is the first thing you do when your doing a Wizard's Duel?"),
+        new Question(6, "6. What is the first thing you do when your in a Wizard's Duel?"),
         new Question(7, "7. You see that Frank is getting bullied by Devin. Frank is really insecure and has really big ears. What do you do?"),
         new Question(8, "8. If you were to fight a dragon, which one would it be?"),
         new Question(9, "9. Which one of these clases do you like the most?"),
@@ -53,7 +53,7 @@ public class Data
             new Dictionary<string, int> { {"Gryffindor", 0}, {"Hufflepuff", 2}, {"Ravenclaw", 4}, {"Slytherin", -1} }),
 
         new Answer(9, 3, "You try to make friends by talking to your fellow classmates.", new Dictionary<string, int> { {"Gryffindor", 2}, {"Hufflepuff", 3}, {"Ravenclaw", -1}, {"Slytherin", 1} }),
-        new Answer(10, 3, "You're curious about the lessons, so you start reading your books already.", new Dictionary<string, int> { {"Gryffindor", 1}, {"Hufflepuff", 0}, {"Ravenclaw", 3}, {"Slytherin", -1} }),
+        new Answer(10, 3, "You're curious about the lessons, so you start by going to the library.", new Dictionary<string, int> { {"Gryffindor", 1}, {"Hufflepuff", 0}, {"Ravenclaw", 3}, {"Slytherin", -1} }),
         new Answer(11, 3, "You explore the castle and look for secrets it hides.", new Dictionary<string, int> { {"Gryffindor", 3}, {"Hufflepuff", 0}, {"Ravenclaw", -1}, {"Slytherin", 1} }),
         new Answer(12, 3, "You want to be the funny guy, so you try to prank someone", new Dictionary<string, int> { {"Gryffindor", -1}, {"Hufflepuff", 0}, {"Ravenclaw", 1}, {"Slytherin", 3} }),
 
@@ -70,7 +70,7 @@ public class Data
         new Answer(21, 6, "You wait for your opponent to cast a spell first, but you're prepared to use Protego so get a shield that will deflect the opponents spell", 
             new Dictionary<string, int> { {"Gryffindor", 0}, {"Hufflepuff", 1}, {"Ravenclaw", 3}, {"Slytherin", 0} }),
         new Answer(22, 6, "you use Stupefy to stun your opponent.", new Dictionary<string, int> { {"Gryffindor", 3}, {"Hufflepuff", 0}, {"Ravenclaw", 0}, {"Slytherin", 1} }),
-        new Answer(23, 6, "You use Everte Statum to thows your opponent backwards.",
+        new Answer(23, 6, "You use Everte Statum to throw your opponent backwards.",
             new Dictionary<string, int> { {"Gryffindor", 1}, {"Hufflepuff", -1}, {"Ravenclaw", 0}, {"Slytherin", 3} }),
         new Answer(24, 6, "You use Expelliarmus to disarm your opponent", new Dictionary<string, int> { {"Gryffindor", 0}, { "Hufflepuff", 3} , {"Ravenclaw", 2}, {"Slytherin", -1} }),
 
@@ -78,7 +78,7 @@ public class Data
             new Dictionary<string, int> { {"Gryffindor", -1}, {"Hufflepuff", 1}, {"Ravenclaw", 3}, {"Slytherin", 1 } }),
         new Answer(26, 7, "Screw Frank he is always annoying in class, so you join Devin and say to Frank: Hey Franky, go flap your ears, hopefully you'll fly away.", 
             new Dictionary<string, int> { {"Gryffindor", -1}, {"Hufflepuff", 0}, {"Ravenclaw", 1}, {"Slytherin", 3} }),
-        new Answer(27, 7, "Stand up for Frank and you push Devin on the ground and tell him to leave Frank alone and get of here.",
+        new Answer(27, 7, "Stand up for Frank so you push Devin to the ground and tell him to leave Frank alone.",
             new Dictionary<string, int> { {"Gryffindor", 3}, {"Hufflepuff", 1}, {"Ravenclaw", -1}, {"Slytherin", 0} }),
         new Answer(28, 7, "You ask some someone to get a teacher in case it escalates. Then you ask Devin to stop bullying Frank.", 
             new Dictionary<string, int> { {"Gryffindor", 1}, {"Hufflepuff", 3}, {"Ravenclaw", 0}, {"Slytherin", -1} }),
